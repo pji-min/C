93cf0928cbab806287f3fa565ecea906a364c215
박지민 <pjmlove2008@gmail.com>
2024-05-29 22:34:48 +0900

    별찍기 1 수정

--- 별찍기.c
+++ 별찍기.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main (void) {
-
-int a = 0;
-scanf("%d",&a);
-for(int i =0; i<a; i++){
-    for(int j = 0; j<i; j++){
-    printf("*");
+    
+    int a = 0;
+    scanf("%d",&a);
+    for(int i =0; i<a; i++){
+        for(int j = 0; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
     }
-    printf("\n");
-}
-return 0;
+    return 0;
 }
